b0ed039470877abd314b267dcfb607213f68eed5
Dishant Singh <58480379+seeker53@users.noreply.github.com>
2024-03-14 08:36:26 +0000

    started counter project to understand hooks and their need

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter,setCounter] = useState(0)
+
+  const addValue = ()=>{
+    console.log("clicked",counter)
+    counter = counter + 1
+    setCounter(counter)
+  }
+
+  const decreaseValue = ()=>{
+    console.log("clicked",counter)
+    counter = counter - 1
+    setCounter(counter)
+  }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={decreaseValue}>Decrease value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
