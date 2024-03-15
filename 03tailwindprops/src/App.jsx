8b1391da290168931de8ee9335c0540d65cfd589
Dishant Singh <58480379+seeker53@users.noreply.github.com>
2024-03-15 16:34:54 +0000

    added props to components

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Cards from './components/Cards'
 import './App.css'
 
 function App() {
 
+    let myObj = {
+      userName:'Dishant',
+      age : 23
+    }
+
+    let myArr = [1,2,3]
+
   return (
     <>
-    <h1 className='flex justify-center align-center text-semibold text-black bg-blue-300'>Chai aur react</h1>
+    <h1 className='flex justify-center align-center text-semibold text-black bg-blue-300 mb-5 rounded-md'>Chai aur react</h1>
+    <Cards userName='thisisdishant' btnText='click me'/>
+    <Cards userName='Dishant' />
     </>
   )
 }
