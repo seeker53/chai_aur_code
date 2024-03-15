6551e235949d457ffd98b749fc61c2961afe06dc
Dishant Singh <58480379+seeker53@users.noreply.github.com>
2024-03-15 08:51:02 +0000

    Started with the learning of tailwind and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='flex justify-center align-center text-semibold text-black bg-blue-300'>Chai aur react</h1>
+    </>
+  )
+}
+
+export default App
