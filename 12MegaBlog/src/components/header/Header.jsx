aba8d53d584a37b515e9360447d278c1403f3dcd
seeker53 <dishant.singh16@gmail.com>
2024-06-04 07:51:54 +0530

    implemented components and pages, did some debugging

--- 12MegaBlog/src/components/header/Header.jsx
+++ 12MegaBlog/src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import { Container, Logo, LogoutBtn } from '../index'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 function Header() {
-    const authStatus = useSelector(status => state.auth.status)
+    const authStatus = useSelector(state => state.auth.status)
     const navigate = useNavigate()
     const navItems = [
         {
--- 12MegaBlog/src/components/header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/header/LogoutBtn.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { authService } from '../../appwrite/auth'
+import { AuthService } from '../../appwrite/auth'
 import { logout } from '../../store/authSlice'
 
 function LogoutBtn() {
   const dispatch = useDispatch();
 
   const logoutHandler = () => {
-    authService.logout().then(() => {
+    AuthService.logout().then(() => {
       dispatch(logout())
     })
   }
